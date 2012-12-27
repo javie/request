b4e7bba8cf695c8e2e39f40aedb0e1bed1fecf46
Mior Muhammad Zaki <mior.muhammad@iproperty.com>
2012-12-27 17:42:32 +0800

    Some idea to request object, basically the focus would be to focus on pubsub usage rather than using callback
    Signed-off-by: Mior Muhammad Zaki <mior.muhammad@iproperty.com>

--- request.js
+++ request.js
@@ -1,6 +1,6 @@
 (function () { 'use strict'; 
 
-	var root, Requests, _, api;
+	var root, Requests, _, api, log, ev, caches;
 
 	// Save a reference to the global object (`window` in the browser, `global` on the server)
 	root = this;
@@ -8,21 +8,26 @@
 	// Create a safe reference to Requests object to be used below.
 	Requests = {};
 
+	caches   = {};
+
 	// At this point, we don't have a request wrapper for Node.js implementation. So it would be
 	// best to display an exception.
 	if ('undefined' !== typeof export) {
 		throw new Error("Not supported");
 	}
 
-	if ('undefined' === typeof root.Javie) root.Javie = {};
+	if ('undefined' === typeof root.Javie) {
+		throw new Error("Javie is required before using Javie.Request");
+	}
 
 	root.Javie.Request = Requests;
 
 	// Load all the dependencies
-	_ = root._;
+	log = root.Javie.Logger.make();
+	ev  = root.Javie.Events.make();
+	_   = root._;
 
 	if (!_ && 'undefined' !== typeof require) _ = require('underscore');
-
 	
 	// Map jQuery or Zepto instance, but I'm not really into this dollar sign stuff since in 
 	// this scope we are focusing on the `ajax` method available from these libraries.
@@ -44,10 +49,10 @@
 	 * @type {Object}
 	 */
 	Requests.config = {
-		baseUrl: null,
+		'baseUrl': null,
 		'onError': function onError (data, status) {},
-		beforeSend: function beforeSend (data, status) {},
-		onSuccess: function onSuccess (data, status) {}
+		'beforeSend': function beforeSend (data, status) {},
+		'onSuccess': function onSuccess (data, status) {}
 	};
 
 	/**
@@ -79,10 +84,65 @@
 	 * @return {void}
 	 */
 	Requests.make = function make(name) {
+		var cache, child, childName, parent, self;
+
 		if (!_.isString(name)) name = 'default';
 
+		self = this;
+
+		// If cache is not empty, this mean that make was initiated before.
+		// @todo we should create child instances if the request has been
+		// executed.
+		if (!_.isUndefined(caches[name])) {
+			parent = caches[name];
+
+			// If parent has been executed, we need to create a child instance.
+			if (parent.executed === true) {
+				childName = _.uniqueId(name+'_');
+				child     = self.make(childName);
+
+				// Replicate all parent configuration to child.
+				ev.clone('Request.onError: '+name).to('Request.onError: '+childName);
+				ev.clone('Request.onSuccess: '+name).to('Request.onSuccess: '+childName);
+				ev.clone('Request.beforeSend: '+name).to('Request.beforeSend: '+childName);
+
+				child.put(parent.config);
+
+				return child;
+			}
+
+			return parent;
+		}
+
+		cache = {
+			executed: false,
+			to: function to (url, data) {
+				
+			},
+			get: function get () {
+				var own;
+
+				own = this;
+
+				api.ajax({
+
+				});
+			},
+			config: {
+				'onError': null,
+				'beforeSend': null,
+				'onSuccess': null
+			},
+			put: function put (key, value) {
+				var config = (!_.isString(key) ? key : { key : value });
+
+				this.config = _.defaults(config, this.config);
+			}
+		};
+
+
+
+		return cache;
 	};
-
-
 
 }).call(this);
